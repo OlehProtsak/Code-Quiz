fe07a22c57da4da1a739e3f14fa4f29f9b7d59e9
Oleh Protsak <protsak19@gmail.com>
2023-11-30 23:00:18 +0000

    Established an array of questions for the quiz

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,101 @@
+const questions = [
+    {
+        queston: 'How can you get the total number of arguments passed to a function?',
+        answers: {
+            answer1: 'Using args.length property',
+            answerCorrect: 'Using arguments.length property',
+            answer3: 'Both of the above.',
+            answer4: 'D - None of the above.',
+        }
+    },
+    
+    {
+        queston: 'Which of the following type of variable takes precedence over other if names are same?',
+        answers: {
+            answer1: ' global variable',
+            answerCorrect: 'local variable',
+            answer3: 'Both of the above.',
+            answer4: 'None of the above.',
+        }
+    },
+
+    {
+        queston: 'Which of the following code creates an object?',
+        answers: {
+            answer1: 'let book = Object();',
+            answerCorrect: 'var book = new Object();',
+            answer3: 'var book = new OBJECT();',
+            answer4: 'var book = new Book();',
+        }
+    },
+
+    {
+        queston: 'Which of the following function of Number object returns a string value version of the current number?',
+        answers: {
+            answerCorrect: 'toString()',
+            answer2: 'toFixed()',
+            answer3: 'toLocaleString()',
+            answer4: 'toPrecision()',
+        }
+    },
+
+    {
+        queston: 'Which of the following function of String object splits a String object into an array of strings by separating the string into substrings',
+        answers: {
+            answer1: 'slice()',
+            answerCorrect: 'split()',
+            answer3: 'replace()',
+            answer4: 'search()',
+        }
+    },
+
+    {
+        queston: 'Which of the following function of String object returns a string representing the specified object?',
+        answers: {
+            answer1: 'toLocaleUpperCase()',
+            answer2: 'toUpperCase()',
+            answerCorrect: 'toString()',
+            answer4: 'substring()',
+        }
+    },
+
+    {
+        queston: 'Which of the following function of String object causes a string to be displayed in the specified color as if it were in a <font color="color"> tag?',
+        answers: {
+            answer1: 'fixed()',
+            answerCorrect: 'fontcolor()',
+            answer3: 'blink()',
+            answer4: 'bold()',
+        }
+    },
+
+    {
+        queston: 'Which of the following function of Array object returns the last (greatest) index of an element within the array equal to the specified value, or -1 if none is found?',
+        answers: {
+            answer1: 'indexOf()',
+            answer2: 'join()',
+            answerCorrect: 'lastIndexOf()',
+            answer4: 'map()',
+        }
+    },
+
+    {
+        queston: 'Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?',
+        answers: {
+            answerCorrect: 'unshift()',
+            answer2: 'sort()',
+            answer3: 'splice()',
+            answer4: 'toString()',
+        }
+    },
+
+    {
+        queston: 'Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?',
+        answers: {
+            answerCorrect: 'unshift()',
+            answer2: 'sort()',
+            answer3: 'splice()',
+            answer4: 'toString()',
+        }
+    },
+];
