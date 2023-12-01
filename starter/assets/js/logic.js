12e18d4a479e4a028a203a3f960e16f010f080fd
Oleh Protsak <protsak19@gmail.com>
2023-12-01 22:45:24 +0000

    Moved index.html and highscores.html to the root folder.

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,11 +1,14 @@
 const allQuestions = document.querySelectorAll('[data-type*=question]');
 const startScreen = document.getElementById('start-screen');
 const endScreen = document.getElementById('end-screen');
+const userInput = document.getElementById('initials');
+const submitBtn = document.getElementById('submit');
 const feedBack = document.getElementById('feedback');
 const container = document.querySelector('.wrapper');
 const timer = document.getElementById('time');
 const correctAudio = new Audio('assets/sfx/correct.wav');
 const incorrectAudio = new Audio('assets/sfx/incorrect.wav');
+const highscores = [];
 let currentQuestionIndex = 0;
 let time;
 let intervalId;
@@ -47,6 +50,23 @@ container.addEventListener('click', (e) => {
         };
     };
 });
+
+submitBtn.addEventListener('click', () => {
+    if (userInput.value) {
+    const newScore = {
+        initial: userInput.value,
+        score: time
+    };
+        highscores.push(newScore);
+        localStorage.setItem('highscores', JSON.stringify(highscores));
+        endScreen.classList.add('hide');
+
+        window.location.href = 'highscores.html';
+    } else {
+        alert('please enter something');
+    }   
+});
+
 
 function hideQuestion(currentIndex) {
     allQuestions[currentIndex].classList.add('hide');
