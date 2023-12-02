96dd68bf897a87bb4e9f06c603e81073ff0f9f37
Oleh Protsak <protsak19@gmail.com>
2023-12-02 00:38:31 +0000

    Implemented a function to clear local storage

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,9 +1,15 @@
 const ol = document.getElementById('highscores');
-const results = JSON.parse(localStorage.getItem('highscores')).sort((a,b) => b.score - a.score);
-console.log(results);
+const clearBtn = document.getElementById('clear');
+const results = JSON.parse(localStorage.getItem('highscores')).sort((a,b) => b.score - a.score) || [];
+
 for (let i = 0; i < results.length; i++) {
     const li = document.createElement('li');
     li.textContent = `${results[i].initials} - ${results[i].score}`;
     ol.appendChild(li);
 }
+
+clearBtn.addEventListener('click', () =>{
+    localStorage.clear();
+    ol.innerText = '';
+});
 
